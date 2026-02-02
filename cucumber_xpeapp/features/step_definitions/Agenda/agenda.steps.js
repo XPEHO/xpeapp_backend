645fa51c23eeba0fee26862ffda3223600633140
Théo Lebègue <theo.lebegue@icloud.com>
2026-02-02 11:06:42 +0100

    feat(agendaSteps): refactor response assertions into reusable functions for improved readability and maintainability

--- cucumber_xpeapp/features/step_definitions/Agenda/agenda.steps.js
+++ cucumber_xpeapp/features/step_definitions/Agenda/agenda.steps.js
@@ -3,6 +3,39 @@ const { apiGet, apiPost, apiPut, apiDelete } = require('../support/httpHelpers')
 const { assertStatus, assertArray, assertToken, assertField, assertNotFoundError } = require('../support/assertHelpers');
 
 // =============================
+// GENERIC RESPONSE ASSERTIONS
+// =============================
+
+function assertListResponse(context, fields) {
+  assertStatus(context.response, 200);
+  assertArray(context.body);
+  if (context.body.length > 0) for (const f of fields) assertField(context.body[0], f);
+  assertToken(context);
+}
+
+function assertDetailResponse(context, fields) {
+  assertStatus(context.response, 200);
+  for (const f of fields) assertField(context.body, f);
+  assertToken(context);
+}
+
+function assertCreated(context) {
+  assertStatus(context.response, 201);
+  assertToken(context);
+}
+
+function assertNoContent(context) {
+  assertStatus(context.response, 204);
+  assertToken(context);
+}
+
+function assertNotFound(context) {
+  assertStatus(context.response, 404);
+  assertNotFoundError(context.body);
+  assertToken(context);
+}
+
+// =============================
 // EVENTS TYPES API STEPS
 // =============================
 
@@ -15,20 +48,11 @@ When('I fetch the event type by the {int}', async function (id) {
 });
 
 Then('I receive a list of event types', function () {
-  assertStatus(this.response, 200);
-  assertArray(this.body);
-  if (this.body.length > 0) {
-    assertField(this.body[0], 'id');
-    assertField(this.body[0], 'label');
-  }
-  assertToken(this);
+  assertListResponse(this, ['id', 'label']);
 });
 
 Then('I receive an event type detail', function () {
-  assertStatus(this.response, 200);
-  assertField(this.body, 'id');
-  assertField(this.body, 'label');
-  assertToken(this);
+  assertDetailResponse(this, ['id', 'label']);
 });
 
 When('I create an event type with label {string} and color_code {string}', async function (label, color_code) {
@@ -36,8 +60,7 @@ When('I create an event type with label {string} and color_code {string}', async
 });
 
 Then('I receive a confirmation of event type creation', function () {
-  assertStatus(this.response, 201);
-  assertToken(this);
+  assertCreated(this);
 });
 
 When('I update event type with id {int} to label {string} and color_code {string}', async function (id, label, color_code) {
@@ -45,8 +68,7 @@ When('I update event type with id {int} to label {string} and color_code {string
 });
 
 Then('I receive a confirmation of event type update', function () {
-  assertStatus(this.response, 204);
-  assertToken(this);
+  assertNoContent(this);
 });
 
 When('I delete event type with id {int}', async function (id) {
@@ -54,8 +76,7 @@ When('I delete event type with id {int}', async function (id) {
 });
 
 Then('I receive a confirmation of event type deletion', function () {
-  assertStatus(this.response, 204);
-  assertToken(this);
+  assertNoContent(this);
 });
 
 // =============================
@@ -67,14 +88,7 @@ When('I fetch the events page {int}', async function (page) {
 });
 
 Then('I receive a list of events', function () {
-  assertStatus(this.response, 200);
-  assertArray(this.body);
-  if (this.body.length > 0) {
-    assertField(this.body[0], 'title');
-    assertField(this.body[0], 'date');
-    assertField(this.body[0], 'type_id');
-  }
-  assertToken(this);
+  assertListResponse(this, ['title', 'date', 'type_id']);
 });
 
 When('I fetch the event with id {int}', async function (id) {
@@ -82,12 +96,7 @@ When('I fetch the event with id {int}', async function (id) {
 });
 
 Then('I receive an event detail', function () {
-  assertStatus(this.response, 200);
-  assertField(this.body, 'id');
-  assertField(this.body, 'title');
-  assertField(this.body, 'date');
-  assertField(this.body, 'type_id');
-  assertToken(this);
+  assertDetailResponse(this, ['id', 'title', 'date', 'type_id']);
 });
 
 When('I create an event with title {string}, date {string}, type_id {string}', async function (title, date, type_id) {
@@ -95,8 +104,7 @@ When('I create an event with title {string}, date {string}, type_id {string}', a
 });
 
 Then('I receive a confirmation of event creation', function () {
-  assertStatus(this.response, 201);
-  assertToken(this);
+  assertCreated(this);
 });
 
 When('I update event with id {int} to title {string}, date {string}, type_id {string}', async function (id, title, date, type_id) {
@@ -104,8 +112,7 @@ When('I update event with id {int} to title {string}, date {string}, type_id {st
 });
 
 Then('I receive a confirmation of event update', function () {
-  assertStatus(this.response, 204);
-  assertToken(this);
+  assertNoContent(this);
 });
 
 When('I delete event with id {int}', async function (id) {
@@ -113,14 +120,11 @@ When('I delete event with id {int}', async function (id) {
 });
 
 Then('I receive a confirmation of deletion', function () {
-  assertStatus(this.response, 204);
-  assertToken(this);
+  assertNoContent(this);
 });
 
 Then('I receive a not found error for event', function () {
-  assertStatus(this.response, 404);
-  assertNotFoundError(this.body);
-  assertToken(this);
+  assertNotFound(this);
 });
 
 // =============================
@@ -132,13 +136,7 @@ When('I fetch the birthdays page {int}', async function (page) {
 });
 
 Then('I receive a list of birthdays', function () {
-  assertStatus(this.response, 200);
-  assertArray(this.body);
-  if (this.body.length > 0) {
-    assertField(this.body[0], 'first_name');
-    assertField(this.body[0], 'birthdate');
-  }
-  assertToken(this);
+  assertListResponse(this, ['first_name', 'birthdate']);
 });
 
 When('I fetch the birthday with id {int}', async function (id) {
@@ -146,11 +144,7 @@ When('I fetch the birthday with id {int}', async function (id) {
 });
 
 Then('I receive a birthday detail', function () {
-  assertStatus(this.response, 200);
-  assertField(this.body, 'id');
-  assertField(this.body, 'first_name');
-  assertField(this.body, 'birthdate');
-  assertToken(this);
+  assertDetailResponse(this, ['id', 'first_name', 'birthdate']);
 });
 
 When('I create a birthday with first name {string}, birthdate {string}, email {string}', async function (firstName, birthdate, email) {
@@ -158,8 +152,7 @@ When('I create a birthday with first name {string}, birthdate {string}, email {s
 });
 
 Then('I receive a confirmation of creation', function () {
-  assertStatus(this.response, 201);
-  assertToken(this);
+  assertCreated(this);
 });
 
 When('I update birthday with id {int} to first name {string}, birthdate {string}, email {string}', async function (id, firstName, birthdate, email) {
@@ -167,8 +160,7 @@ When('I update birthday with id {int} to first name {string}, birthdate {string}
 });
 
 Then('I receive a confirmation of update', function () {
-  assertStatus(this.response, 204);
-  assertToken(this);
+  assertNoContent(this);
 });
 
 When('I delete birthday with id {int}', async function (id) {
@@ -181,7 +173,5 @@ Then('I receive an error response', function () {
 });
 
 Then('I receive a not found error', function () {
-  assertStatus(this.response, 404);
-  assertNotFoundError(this.body);
-  assertToken(this);
+  assertNotFound(this);
 });
